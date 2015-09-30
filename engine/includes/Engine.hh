ff60735f9cc1d5ca3fb306f0a9e95c995c738678
Guillaume Tassery <guillaumetassery@gmail.com>
2015-09-30 23:19:47 +0900

    new: add scene loader

--- engine/includes/Engine.hh
+++ engine/includes/Engine.hh
@@ -1,14 +1,48 @@
 #pragma once
 
+#include <stack>
 #include "Singleton.hh"
 #include "Log.hh"
 #include "SystemManager.hh"
 #include "EntityManager.hh"
 #include "ComponentManager.hh"
+#include "../lib/json/json.h"
+#include "../lib/IJsonSerializable.hh"
 
 namespace StrawberryMilk {
 
   class Engine {
+
+  private:
+    class SceneLoader : public IJsonSerializable {
+
+    public:
+      SceneLoader() {};
+      ~SceneLoader() {};
+
+    public:
+      void Serialize(Json::Value &root) { };
+
+      void Deserialize(Json::Value &root) {
+        for (auto e : root["System"]) {
+          std::cout << "ol" << std::endl;
+          mSystem.push(std::make_pair(e["name"].asString(), e["dll"].asString()));
+        }
+        for (auto e : root["Component"]) {
+          mComponent.push(std::make_pair(e["name"].asString(), e["dll"].asString()));
+        }
+      };
+
+    public:
+      inline std::stack<std::pair<std::string, std::string>> &getComponent() { return mComponent; };
+      inline std::stack<std::pair<std::string, std::string>> &getSystem() { return mSystem; };
+      inline std::list<std::stack<std::string>> &getEntity() { return mEntity; }
+
+    private:
+      std::stack<std::pair<std::string, std::string>> mComponent;
+      std::stack<std::pair<std::string, std::string>> mSystem;
+      std::list<std::stack<std::string>> mEntity;
+    };
 
   public:
     Engine();
@@ -25,6 +59,9 @@ namespace StrawberryMilk {
     void init();
     void run();
     void stop();
+
+  private:
+    void loadScene(StrawberryMilk::Engine::SceneLoader &);
 
   private:
     void loadScene(std::string const &);
--- engine/src/ecs/Engine.cc
+++ engine/src/ecs/Engine.cc
@@ -2,6 +2,7 @@
 #include <utility>
 #include "../../includes/Engine.hh"
 #include "../../system/System.hh"
+#include "../../lib/JsonLoader.hh"
 
 StrawberryMilk::Engine::Engine() : mContinue(true) {}
 StrawberryMilk::Engine::~Engine() {}
@@ -11,6 +12,7 @@ void StrawberryMilk::Engine::stop() {
 }
 
 void StrawberryMilk::Engine::run() {
+
 
   while (mContinue) {
 	  mSystem.updateAllSystem([](StrawberryMilk::System *system) {
@@ -25,8 +27,37 @@ void StrawberryMilk::Engine::run() {
   });
 }
 
+void StrawberryMilk::Engine::loadScene(StrawberryMilk::Engine::SceneLoader &scene) {
+  {
+    auto e = scene.getSystem();
+
+    while (!e.empty()) {
+      std::pair<std::string, std::string> system = e.top();
+      mSystem.insertSystem(system.first, system.second);
+      e.pop();
+    }
+  }
+  {
+    auto e = scene.getComponent();
+
+    while (!e.empty()) {
+      std::pair<std::string, std::string> component = e.top();
+      mComponent.loadComponent(component.first, component.second);
+      e.pop();
+    }
+  }
+}
+
 void StrawberryMilk::Engine::init() {
+   JsonLoader jl("./engine/config/scene/startup-scene.json");
+   StrawberryMilk::Engine::SceneLoader scene;
+
+   jl.LoadFile();
+   jl.loadObject(&scene);
+   this->loadScene(scene);
+//   jl.showContent();
 }
 
 void StrawberryMilk::Engine::loadScene(std::string const &path) {
+
 }
