6807a49a647910821cdd7ca98594d7e6cc6f640a
Yiurule <guillaumetassery@gmail.com>
2015-10-07 13:21:34 +0200

    new: update template

--- engine/ressource/system/test/test.hpp
+++ engine/ressource/system/test/test.hpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include "../../../system/System.hh"
 #include "../../../includes/Engine.hh"
+#include "../../../component/Component.hh"
 
 struct __declspec(dllexport) Test : public StrawberryMilk::System {
 
@@ -13,5 +14,5 @@ struct __declspec(dllexport) Test : public StrawberryMilk::System {
   void init(StrawberryMilk::Engine *) { std::cout << "MY COMPONENT HAS INIT"; };
   void update(std::chrono::duration<double> /* */, StrawberryMilk::Engine * /* */) { std::cout << "MY COMPONENT WAS UPDATE"; };
   void destroy(StrawberryMilk::Engine * /* */) { std::cout << "MY COMPONENT WAS DESTROY"; };
-  bool registerEntity() { return true;  };
+  bool registerEntity(StrawberryMilk::Component::Type::IDComponent /* */) { return true;  };
 };
